564dd7e2de642702c68c04859049e79ac68b4dd6
phucbone <phuc.bone@gmail.com>
2025-04-08 18:50:10 +0700

    Update

--- scripts/env_create.sh
+++ scripts/env_create.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 if [ -z "$NUMBER" ]; then
-    if [ -z "$1" ]; then
-        echo "Không tìm thấy biến môi trường NUMBER hoặc tham số đầu vào."
-        exit 1
-    else
-        INPUT_NUMBER=$1
-    fi
+  if [ -z "$1" ]; then
+    echo "Không tìm thấy biến môi trường NUMBER hoặc tham số đầu vào."
+    exit 1
+  else
+    INPUT_NUMBER=$1
+  fi
 else
-    INPUT_NUMBER=$NUMBER
+  INPUT_NUMBER=$NUMBER
 fi
 
 OUTPUT=""
-for ((i=1; i<=INPUT_NUMBER; i++)); do
-    HOSTNAME_INDEX=$((i-1))
-    ENTRY="${i}@hostname-${HOSTNAME_INDEX}"
-    if [ -z "$OUTPUT" ]; then
-        OUTPUT="$ENTRY"
-    else
-        OUTPUT+=" , $ENTRY"
-    fi
+for ((i = 1; i <= INPUT_NUMBER; i++)); do
+  HOSTNAME_INDEX=$((i - 1))
+  ENTRY="${i}@hostname-${HOSTNAME_INDEX}"
+  if [ -z "$OUTPUT" ]; then
+    OUTPUT="$ENTRY"
+  else
+    OUTPUT+=" , $ENTRY"
+  fi
 done
 
 echo "$OUTPUT"
